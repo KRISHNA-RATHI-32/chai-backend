c391267e3008fbd2f29639e5f9eba67d3110d284
KRISHNA-RATHI-32 <krishna.r1042006@gmail.com>
2025-10-09 22:02:49 +0530

    lecture 14

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@
 // asyncHandler is usually a higher-order function that helps handle errors
 // in async route handlers without needing to use try-catch everywhere.
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/apiError.js";
+import {User} from "../models/user.model.js";
 
 // Defining an asynchronous route handler function called `registerUser`
 // We wrap this function using `asynchandler()` so that if any error occurs inside it,
@@ -10,9 +12,45 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // Inside this async function, we can write our registration logic.
     // For now, it just sends a JSON response with HTTP status 200 and message "ok".
+    //sample below is for check
     res.status(200).json({
         message: "ok"
     });
+    const {fullName,email,username,password}=req.body;
+    console.log("email",email);
+    // if(fullName==="" || email==="" || username==="" || password===""){
+    //     res.status(400);
+    //     throw new ApiError("All fields are mandatory");
+    // }
+    if([fullName,email,username,password].some((field)=>field?.trim()==="")){
+        
+        throw new ApiError(400,"All fields are mandatory");
+    }
+    //how to check if user already exists
+    //first import user model.js
+   const existedUser= User.findOne({
+        $or:[
+            { email},
+            { username}
+        ]
+    })
+    if(existedUser){
+        throw new ApiError(409,"User already exists with this email or username");
+    }
+    
+
+    
+    
+    
+    //how to register user will be implemented here
+    //get user details from frontend we can do it by postman
+    //validation for data-(not all empty)
+    //check if user already exists in db we can do this by checkin email or username
+    //check for images,check for avatar if available upload it to cloudinary also then check if properly uploaded 
+    //create user object -create entry in db
+    //remove password and refresh token field from response
+    //check for user creation success 
+    //return success response to frontend
 });
 
 // Exporting registerUser (not shown here, but usually you’d do something like: export { registerUser })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router=Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        {name:"avatar",
+            maxCount:1//the maximum number of files that can be uploaded with this field name
+        },
+        {name:"coverimage",
+            maxCount:1
+        }
+    ]),
+    registerUser);
 // router.route("/login").post()//login route,it is declared but not implemented as we have not created login function in controller
 
 
